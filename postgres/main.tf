9daa5789177e6d80e0482e97bd7d52263af06bad
will Farrell <will.farrell@gmail.com>
2018-06-27 08:45:25 -0600

    docs: improve comments

--- postgres/main.tf
+++ postgres/main.tf
@@ -1,4 +1,6 @@
 resource "aws_db_instance" "master" {
+  auto_minor_version_upgrade      = true
+  allow_major_version_upgrade     = false
   allocated_storage               = "${var.allocated_storage}"
   identifier                      = "${var.name}"
   storage_type                    = "${var.storage_type}"
@@ -8,20 +10,9 @@ resource "aws_db_instance" "master" {
   name                            = "${var.db_name}"
   parameter_group_name            = "${var.parameter_group_name}"
   apply_immediately               = true
-  # Scalibility
-  multi_az                        = "${var.multi_az}"
-  # Integrity
-  final_snapshot_identifier       = "${var.name}"
-  backup_retention_period         = "${var.backup_retention_period}"
-  backup_window                   = "${var.backup_window}"
-  # Auditiability
-  enabled_cloudwatch_logs_exports = "audit"
-  # TODO add in `monitoring_interval` & `monitoring_role_arn`
-  # Security
+  # Confidentiality
   username                        = "${var.username}"
   password                        = "${var.password}"
-  auto_minor_version_upgrade      = true
-  allow_major_version_upgrade     = false
   publicly_accessible             = false
   db_subnet_group_name            = "${aws_db_subnet_group.db_subnet.name}"
   vpc_security_group_ids          = [
@@ -30,8 +21,18 @@ resource "aws_db_instance" "master" {
   # TODO research and apply `kms_key_id`
 
   # TODO add in `storage_encrypted`
+  # Integrity
+  enabled_cloudwatch_logs_exports = "audit"
+  # TODO add in `monitoring_interval` & `monitoring_role_arn`
+  final_snapshot_identifier       = "${var.name}"
+  backup_retention_period         = "${var.backup_retention_period}"
+  backup_window                   = "${var.backup_window}"
+  # Auditiability
+  multi_az                        = "${var.multi_az}"
+
 }
 
-
-
 # TODO add in replicate db with `replicate_source_db`
+//resource "aws_db_instance" "slave" {
+//  replicate_source_db = "${aws_db_instance.master.name}"
+//}
