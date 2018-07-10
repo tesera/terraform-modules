ccf5011f883de644ca6bbfc8eed3b2e188f809fe
will Farrell <will.farrell@gmail.com>
2018-07-10 10:46:44 -0600

    docs: return role

--- bastion/output.tf
+++ bastion/output.tf
@@ -2,6 +2,10 @@ output "public_ip" {
   value = "${aws_eip.main.public_ip}"
 }
 
+output "iam_role_name" {
+  value = "${aws_iam_role.main.name}"
+}
+
 output "security_group_id" {
   value = "${aws_security_group.main.id}"
 }
