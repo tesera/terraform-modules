19cffbd262621db3e4193d91e36415c71e6107d0
will Farrell <will.farrell@gmail.com>
2018-09-10 08:19:22 -0600

    fix: separate out nat

--- vpc/subnet_public.tf
+++ vpc/subnet_public.tf
@@ -26,29 +26,6 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_eip" "nat" {
-  count = "${local.az_count}"
-  vpc   = true
-
-  tags {
-    Name      = "${var.name}-az-${local.az_name[count.index]}"
-    Terraform = "true"
-  }
-}
-
-resource "aws_nat_gateway" "public" {
-  count         = "${local.az_count}"
-  allocation_id = "${aws_eip.nat.*.id[count.index]}"
-  subnet_id     = "${aws_subnet.public.*.id[count.index]}"
-
-  tags {
-    Name      = "${var.name}-az-${local.az_name[count.index]}"
-    Terraform = "true"
-    Environment = "${var.environment}"
-  }
-}
-
-
 resource "aws_route_table_association" "public" {
   count          = "${local.az_count}"
   subnet_id      = "${aws_subnet.public.*.id[count.index]}"
