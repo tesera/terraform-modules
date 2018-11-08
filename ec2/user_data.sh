767e27b6837f0f0915e3165e6f4b131a514901cb
kkirov <kiril.t.kirov@gmail.com>
2018-11-08 09:51:09 -0700

    fix: cleanup ec2 user data.

--- ec2/user_data.sh
+++ ec2/user_data.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-echo "***** Instance ENV *****"
-INSTANCE_ID=$(curl -s -m 60 http://169.254.169.254/latest/meta-data/instance-id)
-
 echo "***** Update *****"
 yum update -y
 pip install --upgrade awscli
