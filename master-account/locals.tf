02b2527ab8183d6068bb17ce6b76a3fc8bf81f52
will Farrell <will.farrell@gmail.com>
2018-11-14 06:17:46 -0700

    fix: remove unused vars

--- master-account/locals.tf
+++ master-account/locals.tf
@@ -13,7 +13,6 @@ module "defaults" {
 locals {
   account_id               = "${module.defaults.account_id}"
   aws_region               = "${module.defaults.aws_region}"
-  name                     = "${module.defaults.name}"
   account_email_local_part = "${element(split("@", var.account_email),0)}"
   account_email_domain     = "${element(split("@", var.account_email),1)}"
   groups                   = "${data.external.groups.result}"
