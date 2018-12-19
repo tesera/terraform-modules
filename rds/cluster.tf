138ccf45f23378b8d2d2d47707ed3c0b076ce387
kkirov <kiril.t.kirov@gmail.com>
2018-12-19 13:04:38 -0700

    feature: add ability to use IAM authentication with RDS

--- rds/cluster.tf
+++ rds/cluster.tf
@@ -1,22 +1,23 @@
 resource "aws_rds_cluster" "main" {
-  count                           = "${var.type == "cluster" ? 1 : 0}"
-  cluster_identifier              = "${local.identifier}-cluster"
-  database_name                   = "${var.db_name}"
-  master_username                 = "${var.username}"
-  master_password                 = "${var.password}"
-  final_snapshot_identifier       = "${local.identifier}-final"
-  skip_final_snapshot             = "${var.skip_final_snapshot}"
-  engine                          = "${var.cluster_engine}"
-  engine_version                  = "${var.engine_version}"
-  engine_mode                     = "${var.engine_mode}"
-  storage_encrypted               = "true"
-  db_subnet_group_name            = "${aws_db_subnet_group.main.name}"
-  vpc_security_group_ids          = ["${aws_security_group.main.id}"]
-  db_cluster_parameter_group_name = "${var.parameter_group_name}"
-  vpc_security_group_ids          = ["${aws_security_group.main.id}"]
-  backup_retention_period         = "${var.backup_retention_period}"
-  preferred_backup_window         = "${var.backup_window}"
-  apply_immediately               = "${var.apply_immediately}"
+  count                               = "${var.type == "cluster" ? 1 : 0}"
+  cluster_identifier                  = "${local.identifier}-cluster"
+  database_name                       = "${var.db_name}"
+  master_username                     = "${var.username}"
+  master_password                     = "${var.password}"
+  final_snapshot_identifier           = "${local.identifier}-final"
+  skip_final_snapshot                 = "${var.skip_final_snapshot}"
+  engine                              = "${var.cluster_engine}"
+  engine_version                      = "${var.engine_version}"
+  engine_mode                         = "${var.engine_mode}"
+  storage_encrypted                   = "true"
+  db_subnet_group_name                = "${aws_db_subnet_group.main.name}"
+  vpc_security_group_ids              = ["${aws_security_group.main.id}"]
+  db_cluster_parameter_group_name     = "${var.parameter_group_name}"
+  vpc_security_group_ids              = ["${aws_security_group.main.id}"]
+  backup_retention_period             = "${var.backup_retention_period}"
+  preferred_backup_window             = "${var.backup_window}"
+  apply_immediately                   = "${var.apply_immediately}"
+  iam_database_authentication_enabled = "${var.iam_database_authentication_enabled}"
 
   tags {
     Name      = "${local.identifier} Aurora Cluster"
