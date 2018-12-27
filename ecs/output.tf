563ea153e1a6a67264779ae31eb09b6ba0dc064a
will Farrell <will.farrell@gmail.com>
2018-12-27 16:56:38 -0700

    fix: add arn to output

--- ecs/output.tf
+++ ecs/output.tf
@@ -11,6 +11,10 @@ output "iam_role_name" {
   value = "${module.ec2.iam_role_name}"
 }
 
+output "iam_role_arn" {
+  value = "${module.ec2.iam_role_arn}"
+}
+
 output "security_group_id" {
   value = "${module.ec2.security_group_id}"
 }
