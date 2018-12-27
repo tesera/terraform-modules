2bf1d7c2e0e6317d2e4d0eeeb6ec705fe0702093
will Farrell <will.farrell@gmail.com>
2018-12-27 10:31:08 -0700

    fix: add cluster_id

--- ecs/output.tf
+++ ecs/output.tf
@@ -2,6 +2,10 @@ output "name" {
   value = "${aws_ecs_cluster.main.name}"
 }
 
+output "id" {
+  value = "${aws_ecs_cluster.main.id}"
+}
+
 # EC2 Output
 output "iam_role_name" {
   value = "${module.ec2.iam_role_name}"
