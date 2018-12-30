c39ec7dabbef44a4ddd128b26eedeccaac6412db
will Farrell <will.farrell@gmail.com>
2018-12-30 15:34:42 -0700

    fix: linting

--- defaults/locals.tf
+++ defaults/locals.tf
@@ -2,8 +2,8 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 locals {
-  account_id = "${data.aws_caller_identity.current.account_id}"
-  profile    = "${data.aws_caller_identity.current.user_id}"
+  account_id        = "${data.aws_caller_identity.current.account_id}"
+  profile           = "${data.aws_caller_identity.current.user_id}"
   region            = "${data.aws_region.current.name}"
   name              = "${replace(var.name, "/[^a-zA-Z0-9-]/", "-")}"
   name_alphanumeric = "${replace(var.name, "/[^a-zA-Z0-9]/", "")}"
