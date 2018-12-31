9cb5539a191f1d763a8852e6281d1041471b5aec
will Farrell <will.farrell@gmail.com>
2018-12-31 12:07:29 -0700

    fix: param group cluster options

--- elasticache/locals.tf
+++ elasticache/locals.tf
@@ -8,7 +8,7 @@ locals {
   name                 = "${module.defaults.name}"
   tags                 = "${module.defaults.tags}"
   engine_family        = "${var.engine}${var.engine_version}"
-  parameter_group_name = "${var.parameter_group_name != "" ? var.parameter_group_name : "default.${local.engine_family}" }"
+  parameter_group_name = "${var.parameter_group_name != "" ? var.parameter_group_name : "default.${local.engine_family}"}${(var.type == "cluster" && var.engine == "redis") ? ".cluster.on" : ""}"
 
   master_endpoint = "${element(concat(aws_elasticache_replication_group.cluster.*.configuration_endpoint_address, aws_elasticache_replication_group.service.*.primary_endpoint_address), 0)}"
   master_len      = "${length(element(split(".", local.master_endpoint), 0))}"
